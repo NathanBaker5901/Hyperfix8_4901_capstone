adfb8f283dfde32cc02a01125342fbcf7d0e44be
Carlos Garcia <cg6029847@gmail.com>
2024-10-25 23:28:16 -0500

    Color
    Added the box to be more rounded will work on the positions of the buttons

--- Main.py
+++ Main.py
@@ -3,28 +3,30 @@ from kivy.uix.button import Button #for the buttons
 from kivy.uix.boxlayout import BoxLayout # for the layout (there are different types of layouts in kivy this is just the most basic one)
 from kivy.uix.screenmanager import ScreenManager, Screen #for the screen manager to track which screen is being shown 
 from kivy.uix.label import Label # for the label like headers 
-
+from kivy.uix.widget import Widget # adds widget for each of the classes
+from kivy.graphics import Color, Ellipse # adds color to the circle
+from kivy.core.window import Window 
 #class for the Front page child function of the ScreenManager class imported above
-class FrontPage(Screen):
+class FrontPage(Screen, Widget):
     #constructor for the front page screen
     pass
 
 #class for the settings page
-class SettingsPage(Screen):
+class SettingsPage(Screen, Widget):
     pass
     
 #class for the camera page
-class CameraPage(Screen):
+class CameraPage(Screen, Widget):
     pass
 
     #constructor for the camera page   
 
 #class for the manual page
-class ManualPage(Screen):
+class ManualPage(Screen, Widget):
     pass
 
 #main function for the app
-class MainApp(App):
+class MainApp(App, Widget):
 
     #function for the root of the of the widgets where the apps UI will be added 
     def build(self):
