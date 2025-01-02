c6efd00e7a39c6d36ff958df2dce6ef85eac0644
NathanBaker5901 <nathanbakercs@gmail.com>
2025-01-02 17:55:19 -0600

    deleted SDK folder

--- sdk/build/intermediates/cxx/Debug/176q5r6d/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\nated\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\nated\\Documents\\GitHub\\Hyperfix8_4901_capstone\\sdk\\.cxx\\Debug\\176q5r6d\\armeabi-v7a" ^
-  opencv_jni_shared
